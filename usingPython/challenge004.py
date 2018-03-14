d0d4ee86990fda51836538e3ee4892788588b15a
Brent Young <brentbenyoung@gmail.com>
2018-03-13 20:55:20 -0500

    Managed to fit a quick excersise in.

---
+++ usingPython/challenge004.py
@@ -0,0 +1,5 @@
+#Create a short quiz which asks the user a question, and says "Well done :)" if the question is answered correctly.
+
+answer = input("What color is grass?")
+if answer == "green" or "Green":
+    print("Well done:)")
